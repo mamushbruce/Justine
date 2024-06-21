20c28f6f9f7cff1ade13abd33d9e4bfdc34464c5
mamushbruce <156106657+mamushbruce@users.noreply.github.com>
2024-06-21 19:43:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "memory-game-cache-v1";
+const CACHE_NAME = "memory-game-cache-v2";
+const dynamicCacheName = 'site-dynamic-v1';
 const urlsToCache = [
   "/",
   "/index.html",
@@ -8,6 +9,8 @@ const urlsToCache = [
   "/prefixfree.min.js",
   "/script.js",
   "/manifest.json",
+  "/tr/IMG_20231029_121158.png",
+  "/tr/Screenshot(51).png",
   "/icons/icon-96x96.png",
   "/icons/icon-512x512.png",
   "https://fonts.googleapis.com/css?family=Open+Sans",
@@ -18,6 +21,17 @@ const urlsToCache = [
   "https://s3-us-west-2.amazonaws.com/s.cdpn.io/60583/general_foundicons.svg#GeneralFoundicons",
 ];
 
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
@@ -26,9 +40,23 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => response || fetch(event.request))
+    // caches
+    //   .match(event.request)
+    //   .then((response) => response || fetch(event.request))
+    caches.match(event.request).then(cacheRes => {
+        return cacheRes || fetch(event.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(event.request.url, fetchRes.clone());
+            // check cached items size
+            limitCacheSize(dynamicCacheName, 15);
+            return fetchRes;
+          })
+        });
+      }).catch(() => {
+        if(event.request.url.indexOf('.html') > -1){
+          return caches.match('/pages/fallback.html');
+        }
+      })
   );
 });
 
